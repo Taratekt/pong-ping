01bb4da8695b952a20bdf377909cb529866bbdb6
Taratekt <savva.morozov1812@gmail.com>
2021-07-24 19:24:36 +0300

    first commit

---
+++ pong.py
@@ -0,0 +1,104 @@
+from pygame import *
+#класс-родитель для других спрайтов
+class GameSprite(sprite.Sprite):
+    #конструктор класса
+    def __init__(self, player_image, player_x, player_y, size_y, player_speed, wight, height):
+        #вызываем конструктор класса (Sprite):
+        super().__init__()
+        #каждый спрайт должен хранить свойство image - изображение
+        self.image = transform.scale(image.load(player_image), (wight, height))
+        self.speed = player_speed
+        #каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    #метод, отрисовывающий героя на окне
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+#класс главного игрока
+class Player(GameSprite):
+  #метод для управления спрайтом стрелками клавиатуры
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_width - 80:
+            self.rect.x += self.speed
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_width - 80:
+            self.rect.x += self.speed
+ 
+   #метод "выстрел" (используем место игрока, чтобы создать там пулю)
+    def fire(self):
+        bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15)
+        bullets.add(bullet)
+back = (200, 255, 255)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+racket1 = Player('platform.png', 30, 200, 4, 50, 150)
+racket2 = Player('platform.png', 520, 200, 4, 50, 150)
+ball = GameSprite('ball.png', 200, 200, 4, 50, 50)
+
+font.init()
+font = font.Font(None, 35)
+lose1 = font('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font('PLAYER 2 LOSE!', True, (180, 0, 0))
+
+speed_x = 3
+speed_y = 3
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y = 1
+
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+            game_over = True
+
+
+        racket1.reset()
+        rakcet2.reset()
+        ball.reset()
+
+    display.update()
+    clock.tick(FPS)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
